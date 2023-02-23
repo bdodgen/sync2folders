dedbced9f1d38a726d76211810c646cc28c51a22
Briana Dodgen <brianadodgen@gmail.com>
2023-02-23 16:52:07 +0100

    cleaned up argparsing again, started log_operation function

--- main.py
+++ main.py
@@ -20,14 +20,20 @@
 # printed to the console
 
 import argparse
+import sys
 import time
 
 # parsing arguments and assigning them to variables for cleanliness
 parser = argparse.ArgumentParser(description='Synchronize replica folder to source folder.')
-parser.add_argument('-s', '--source', type=str, metavar='', required=True, help='filepath of source folder')
-parser.add_argument('-r', '--replica', type=str, metavar='', required=True, help='filepath of replica folder')
-parser.add_argument('-i', '--interval', type=int, metavar='', required=True, help='sync interval in seconds')
-parser.add_argument('-l', '--log', type=str, metavar='', required=True, help='filepath of logfile')
+parser.add_argument('source', type=str, metavar='', nargs='?', help='filepath of source folder')
+parser.add_argument('replica', type=str, metavar='', nargs='?', help='filepath of replica folder')
+parser.add_argument('interval', type=int, metavar='', nargs='?', help='sync interval in seconds')
+parser.add_argument('log', type=str, metavar='', nargs='?', help='filepath of logfile')
+
+parser.add_argument('-s', '--source', type=str, metavar='', dest='source', help='filepath of source folder')
+parser.add_argument('-r', '--replica', type=str, metavar='', dest='replica', help='filepath of replica folder')
+parser.add_argument('-i', '--interval', type=int, metavar='', dest='interval', help='sync interval in seconds')
+parser.add_argument('-l', '--log', type=str, metavar='', dest='log', help='filepath of logfile')
 args = parser.parse_args()
 
 source_path = args.source
@@ -35,7 +41,13 @@ replica_path = args.replica
 sync_interval = args.interval
 log_path = args.log
 
+
+# ------ functions ------
+
 # TODO: create a function for adding to the logfile + printing to console (possibly separate)
+
+def log_operation(operation):
+    print(operation)
 
 # TODO: If log file doesn't exist, create it, print to console. If source folder doesn't exist,
 #  create it, print to console, save to log. If replica folder doesn't exist, create from copy
@@ -56,9 +68,15 @@ log_path = args.log
 #  Do I need something recursive to search through subfolders?
 
 
-# main loop
+# ------ main loop ------
 if __name__ == '__main__':
-    # print(f"Source: {source_path} \n Replica: {replica_path} \n Interval: {sync_interval} \n Logfile: {log_path}")
+    if not (bool(source_path) & bool(replica_path) & bool(sync_interval) & bool(log_path)):
+        print("Please provide a filepath for the source folder, a filepath for the replica folder, a synchronization interval, and a filepath for the log file.")
+        sys.exit()
+
+    print(f"Source: {source_path} \n Replica: {replica_path} \n Interval: {sync_interval} \n Logfile: {log_path}")
+
     while True:
         print(f"Program running. You should see this message again in {sync_interval} seconds.")
+        log_operation(source_path)
         time.sleep(sync_interval)
